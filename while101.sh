08680eae18fafc42aa93cc51b9bd5dee8f2131ba
shazaaly <shaza.aly@gmail.com>
2023-07-27 16:38:01 +0300

    while 2

--- while101.sh
+++ while101.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 counter=5
 
@@ -6,5 +6,14 @@ while [ $counter -ge 1 ];
 do
 	echo "count : $counter"
 	((counter--))
+
+
+done
+num=1
+
+while [ $num -le 3 ];
+do
+	echo "num : $num"
+	((num++))
 
 done
